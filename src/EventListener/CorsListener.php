d6ca08b058f8aabfca227c3a06c52b0843d2ffda
Your Name <you@example.com>
2024-05-21 16:32:08 +0300

    Cors fix

--- src/EventListener/CorsListener.php
+++ src/EventListener/CorsListener.php
@@ -41,7 +41,7 @@ final class CorsListener implements EventSubscriberInterface
         
         $response = $event->getResponse();
         if ($response) {
-            $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:3000');
+            $response->headers->set('Access-Control-Allow-Origin', 'https://frontendngb.onrender.com');
             $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
             $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
             $response->headers->set('Access-Control-Allow-Credentials', 'true'); // Вот это важно
